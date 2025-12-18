785143859711ec0d56b7f48cf9f6433b5c596adc
FilimonovDev <149704053+FilimonovDev@users.noreply.github.com>
2025-12-18 06:51:42 +0300

    feat: game creation menu

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Добро пожаловать в игру Виселица!\n 1.Начать игру\n 2.Выйти из игры")
+	fmt.Print("Введите значение: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan() // Читает ВСЮ строку до \n
+	text := scanner.Text()
+	if text == "1" {
+		fmt.Println("Вы начали игру!")
+	} else if text == "2" {
+		os.Exit(0)
+	} else {
+		fmt.Println("Вы ввели неверное значение")
+	}
+
+}
